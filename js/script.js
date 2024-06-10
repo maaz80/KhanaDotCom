ba8c142ccc674225640c8c8b4eeb6e2fa48bd7b7
maaz80 <maazforlap@gmail.com>
2024-06-10 15:57:21 +0530

    Working

--- js/script.js
+++ js/script.js
@@ -2,3 +2,24 @@ document.getElementById('menu-toggle').addEventListener('click', function () {
     const navMenu = document.getElementById('nav-menu');
     navMenu.classList.toggle('show');
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    let cartCount = 0;
+    const cartCounterElement = document.getElementById('itemincart');
+
+    const addToCartButtons = document.querySelectorAll('.cart-count');
+    addToCartButtons.forEach(button => {
+        let addedToCart = false;
+        button.addEventListener('click', () => {
+            if (!addedToCart) {
+                cartCount++;
+                button.textContent = 'Remove from Cart';
+            } else {
+                cartCount--;
+                button.textContent = 'Add to Cart';
+            }
+            addedToCart = !addedToCart;
+            cartCounterElement.textContent = cartCount;
+        });
+    });
+});

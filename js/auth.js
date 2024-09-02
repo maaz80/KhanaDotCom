0b13a66c2bede9922d3238750a3f51093427d9bc
maaz80 <maazforlap@gmail.com>
2024-09-02 22:37:52 +0530

    Add restaurant and Add Menu Buttons Added in Index page

--- js/auth.js
+++ js/auth.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const loginButtons = document.querySelectorAll(".loginButton");
     const isLoggedIn = localStorage.getItem("isLoggedIn");
+    const token = localStorage.getItem("accessToken");
 
     // Popup elements
     const logoutPopup = document.getElementById("logoutPopup");
@@ -10,6 +11,31 @@ document.addEventListener("DOMContentLoaded", function () {
     loginButtons.forEach((loginButton) => {
         if (isLoggedIn === "true") {
             loginButton.innerHTML = '<a href="#" class="logoutLink">Logout</a>';
+
+            try {
+                fetch('http://13.201.28.236:8000/profile-user/', {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                })
+                .then(response => response.json())
+                .then(data => {
+                    const userType = data.user_type;
+                    if (userType === 'restaurant_owner') {
+                        const restaurantOwnerLinks = document.querySelectorAll('.restaurant-owner-links');
+                        restaurantOwnerLinks.forEach(link => {
+                            link.style.visibility = 'visible';
+                        });
+                    }
+                })
+                .catch(error => {
+                    console.log('There was an error in fetching the Restaurant owner options: ' + error);
+                });
+            } catch (error) {
+                console.error('Error occurred during fetching user type:', error);
+            }
+
         } else {
             loginButton.innerHTML = '<a href="login.html">Login</a>';
         }
@@ -31,7 +57,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Reset button text to "Login" after logging out
         loginButtons.forEach((loginButton) => {
-            loginButton.innerHTML = '<a href="login.html">LogIn</a>';
+            loginButton.innerHTML = '<a href="login.html">Login</a>';
         });
 
         window.location.href = "index.html";
@@ -43,7 +69,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-// Header
+// Header Toggle Functionality
 const navMenu = document.getElementById('nav-menu');
 const filterMenu = document.getElementById('filter-menu');
 const SignupMenu = document.getElementById('sign-menu');
@@ -54,7 +80,7 @@ document.getElementById('menu-toggle').addEventListener('click', function (event
     navMenu.classList.toggle('show');
     filterMenu.classList.remove("show");
     SignupMenu.classList.remove("show");
-    ChatBot.classList.remove("show")
+    ChatBot.classList.remove("show");
 });
 
 document.getElementById('filter-toggle').addEventListener('click', function (event) {
@@ -62,7 +88,7 @@ document.getElementById('filter-toggle').addEventListener('click', function (eve
     filterMenu.classList.toggle('show');
     navMenu.classList.remove("show");
     SignupMenu.classList.remove("show");
-    ChatBot.classList.remove("show")
+    ChatBot.classList.remove("show");
 });
 
 document.getElementById('signup-toggle').addEventListener('click', function (event) {
@@ -70,66 +96,54 @@ document.getElementById('signup-toggle').addEventListener('click', function (eve
     SignupMenu.classList.toggle('show');
     navMenu.classList.remove("show");
     filterMenu.classList.remove("show");
-    ChatBot.classList.remove("show")
+    ChatBot.classList.remove("show");
 });
+
 document.getElementById('bot-toggle').addEventListener('click', function (event) {
     event.stopPropagation();
-    ChatBot.classList.toggle("show")
+    ChatBot.classList.toggle("show");
     SignupMenu.classList.remove('show');
     navMenu.classList.remove("show");
     filterMenu.classList.remove("show");
-    
 });
 
 document.addEventListener('click', function (event) {
-    const navMenu = document.getElementById("nav-menu");
-    const filterMenu = document.getElementById('filter-menu');
-    const SignupMenu = document.getElementById('sign-menu');
-    const ChatBot = document.getElementById('ChatBot-Box');
-
     if (!navMenu.contains(event.target) && !filterMenu.contains(event.target) && !SignupMenu.contains(event.target) && !ChatBot.contains(event.target)) {
         navMenu.classList.remove("show");
         filterMenu.classList.remove("show");
         SignupMenu.classList.remove("show");
-        ChatBot.classList.remove("show")
+        ChatBot.classList.remove("show");
     }
-})
+});
 
+// Cart Value Management
+document.addEventListener('DOMContentLoaded', () => {
+    let cartCount = localStorage.getItem('cartCount') ? parseInt(localStorage.getItem('cartCount')) : 0;
+    const cartCounterElement = document.getElementById('itemincart');
+    cartCounterElement.textContent = cartCount;
 
+    const addToCartButtons = document.querySelectorAll('.cart-count');
 
-// Cart Value
-    document.addEventListener('DOMContentLoaded', () => {
-        // Load the cart count from local storage
-        let cartCount = localStorage.getItem('cartCount') ? parseInt(localStorage.getItem('cartCount')) : 0;
-        const cartCounterElement = document.getElementById('itemincart');
-        cartCounterElement.textContent = cartCount;
-  
-        const addToCartButtons = document.querySelectorAll('.cart-count');
+    addToCartButtons.forEach(button => {
+        let buttonId = button.getAttribute('data-id');
+        buttonId = Number(buttonId);
+        let addedToCart = localStorage.getItem(`addedToCart-${buttonId}`) === 'true';
 
-        addToCartButtons.forEach(button => {
-            let buttonId = button.getAttribute('data-id')
-            buttonId = Number(buttonId)
-            console.log(buttonId);
-            
-            let addedToCart = localStorage.getItem(`addedToCart-${buttonId}`) === 'true';
-            console.log(`Found ${addToCartButtons.length} cart buttons`);
-  
-          button.textContent = addedToCart ? 'Remove' : 'Cart'
-          button.addEventListener('click', () => {
+        button.textContent = addedToCart ? 'Remove' : 'Cart';
+        button.addEventListener('click', () => {
             if (!addedToCart) {
-              cartCount++;
-              button.textContent = 'Remove';
-              localStorage.setItem(`addedToCart-${buttonId}`, 'true')
+                cartCount++;
+                button.textContent = 'Remove';
+                localStorage.setItem(`addedToCart-${buttonId}`, 'true');
             } else {
-              cartCount--;
-              button.textContent = 'Cart';
-              localStorage.setItem(`addedToCart-${buttonId}`, "false")
+                cartCount--;
+                button.textContent = 'Cart';
+                localStorage.setItem(`addedToCart-${buttonId}`, 'false');
             }
             addedToCart = !addedToCart;
-  
-            // Update the cart count in the DOM and local storage
+
             cartCounterElement.textContent = cartCount;
             localStorage.setItem('cartCount', cartCount);
-          });
         });
-      });
+    });
+});
